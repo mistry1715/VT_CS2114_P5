aae447806d7dcf3fd3dd151c2b7ea1f6662177d2
jaydeecorp13 <jaydeecorp13@gmail.com>
2015-10-28 11:56:28 -0400

    Added test package and a TempTest class as a template

---
+++ src/musicpref/test/TempTest.java
@@ -0,0 +1,17 @@
+package musicpref.test;
+
+import org.junit.Test;
+import musicpref.*;
+
+public class TempTest extends student.TestCase {
+
+    public void setUp() {
+        // TODO
+    }
+    
+    @Test
+    public void test() {
+        // TODO
+    }
+
+}
