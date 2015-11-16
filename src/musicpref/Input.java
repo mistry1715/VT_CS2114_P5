fc538406d25ed947f5754cc36d8891315a79e3b9
acan444 <ama2106@vt.edu>
2015-11-16 13:37:13 -0500

    Added Input class

---
+++ src/musicpref/Input.java
@@ -0,0 +1,25 @@
+package musicpref;
+
+/**
+ * This class reads in the input file and starts the program.
+ * @author Alec
+ * @author Julian
+ * @author Matt
+ * @version 2015.11.16
+ */
+public class Input 
+{
+	/**
+	 * The main method for the program.
+	 * @param args Command-line arguments.
+	 */
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public Input() 
+	{
+	}
+
+}
