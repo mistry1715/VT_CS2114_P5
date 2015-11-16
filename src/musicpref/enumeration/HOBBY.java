66db628343bb250480f0bf7e73aab4bae38c9ad7
Jay <jaydeecorp13@gmail.com>
2015-11-16 16:59:40 -0500

    Added javadocs to enums

--- src/musicpref/enumeration/HOBBY.java
+++ src/musicpref/enumeration/HOBBY.java
@@ -1,5 +1,13 @@
 package musicpref.enumeration;
 
+/**
+ * Enumeration for the different student hobbies.
+ * 
+ * @author Alec Alderman (ama2106)
+ * @author Julian Davila (jaydee1)
+ * @author Matthew Scanland (mks2752)
+ * @version 2015.11.16
+ */
 public enum HOBBY {
     READ,
     ART,
--- src/musicpref/enumeration/SORT_METHOD.java
+++ src/musicpref/enumeration/SORT_METHOD.java
@@ -1,5 +1,13 @@
 package musicpref.enumeration;
 
+/**
+ * Enumeration for the different sort methods used by the program.
+ * 
+ * @author Alec Alderman (ama2106)
+ * @author Julian Davila (jaydee1)
+ * @author Matthew Scanland (mks2752)
+ * @version 2015.11.16
+ */
 public enum SORT_METHOD {
     GENRE,
     YEAR,
