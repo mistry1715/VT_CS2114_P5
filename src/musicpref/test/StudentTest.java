ee79afb7e215e88f888e1e57493f5c75250ecfd6
jaydeecorp13 <jaydeecorp13@gmail.com>
2015-11-18 13:31:24 -0500

    Added test classes

---
+++ src/musicpref/test/StudentTest.java
@@ -0,0 +1,47 @@
+package musicpref.test;
+
+import musicpref.*;
+import musicpref.comparators.*;
+import musicpref.enumeration.*;
+
+public class StudentTest extends student.TestCase {
+    // params
+
+    public void setUp() {
+        // TODO
+    }
+
+    // Begin testing here
+
+    public void testConstructor() {
+        // TODO
+    }
+    
+    /**
+     * Test the method compareTo().
+     */
+    public void testCompareTo() {
+        // TODO
+    }
+    
+    /**
+     * Test the method equals().
+     */
+    public void testEquals() {
+        // TODO
+    }
+    
+    /**
+     * Test the method toString().
+     */
+    public void testToString() {
+        // TODO
+    }
+    
+    /**
+     * Test the getter methods.
+     */
+    public void testGetters() {
+        // TODO
+    }
+}
